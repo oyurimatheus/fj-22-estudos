4fbf6da2f77952f8211ebd0a6e21cb6e716b7397
Yuri Matheus <yurimatheusb@hotmail.com>
2018-01-14 13:30:51 -0200

    Criando modelo Usuario

---
+++ src/main/java/br/com/caelum/fj22estudos/model/Usuario.java
@@ -0,0 +1,65 @@
+package br.com.caelum.fj22estudos.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotNull
+    private String nome;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String senha;
+
+    /**
+     * @deprecated hibernate only
+     */
+    public Usuario() {}
+
+    public Usuario(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
